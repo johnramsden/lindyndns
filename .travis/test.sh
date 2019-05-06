365dca99471dc445949d81a80b5134784398fba1
John Ramsden <johnramsden@riseup.net>
2019-05-05 23:02:00 -0700

    Add travis caching

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
 linux() {
-    true
+    mkdir -p ~/.config/lindyndns/
+    cat << EOF > ~/.config/lindyndns/config.toml
+api_token = "${API_KEY}"
+domain = "${TRAVIS_OS_NAME}.${DOMAIN}"
+soa_email = "${EMAIL}"
+EOF
 }
 
 osx() {
     plist='ca.johnramsden.lindyndns.plist'
     launch_daemons='/Library/LaunchDaemons'
 
-    sudo cp "target/${TARGET}/release/${PROJECT_NAME}" '/usr/bin/lindyndns'
-    sudo chmod +x '/usr/bin/lindyndns'
     sudo cp "packaging/macos/${plist}" "${launch_daemons}"
 
     sudo launchctl load "${launch_daemons}/${plist}"
 
     mkdir -p ~/Library/Preferences/lindyndns/
     cat << EOF > ~/Library/Preferences/lindyndns/config.toml
-api_token = \"${API_KEY}\"
-domain = \"${TRAVIS_OS_NAME}.${DOMAIN}\"
-soa_email = \"${EMAIL}\"
+api_token = "${API_KEY}"
+domain = "${TRAVIS_OS_NAME}.${DOMAIN}"
+soa_email = "${EMAIL}"
 EOF
 }
 
@@ -28,7 +31,7 @@ windows() {
 
 test() {
     "${TRAVIS_OS_NAME}"
-    /usr/bin/lindyndns
+    "./target/${TARGET}/release/${PROJECT_NAME}"
 }
 
 test
