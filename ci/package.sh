98880c66d96c8bde6b91d531a0680209987445c1
John Ramsden <johnramsden@riseup.net>
2019-05-11 19:29:07 -0700

    Setup munkipkg

--- ci/package.sh
+++ ci/package.sh
@@ -1,26 +1,57 @@
 #!/bin/sh
 
+set -e
+
+## Setup
+
+tmpdir="$(mktemp -d)"
+
+tag=""
+[ -n "${TRAVIS_TAG}" ] && tag="-${TRAVIS_TAG}"
+
+name="${PROJECT_NAME}${tag}-${TARGET}"
+staging="${tmpdir}/${PROJECT_NAME}"
+
+out_dir="$(pwd)/deployment"
+mkdir -p "${out_dir}" "${staging}"
+
+##
+
 linux() {
     cp "packaging/linux/${PROJECT_NAME}.service" "${staging}"
     cp "packaging/linux/${PROJECT_NAME}.timer" "${staging}"
 }
 
 osx() {
-    true
+    git clone https://github.com/munki/munki-pkg.git munkipkg
+    cd munkipkg
+
+    cp -r "${TRAVIS_BUILD_DIR}/packaging/macos/munkipkg/lindyndns" lindyndns
+
+    payload="lindyndns/payload"
+    plist='ca.johnramsden.lindyndns.plist'
+
+    bindir="${payload}/usr/local/bin"
+    launch_daemons="${payload}/Library/LaunchDaemons"
+
+    mkdir -p "${bindir}" "${launch_daemons}"
+
+    cp "${staging}/lindyndns" "${bindir}"
+    cp "${TRAVIS_BUILD_DIR}/packaging/macos/${plist}" "${launch_daemons}"
+
+
+    (
+        python3 -m venv venv && . venv/bin/activate && \
+        pip install -r requirements.txt && \
+        python3 munkipkg lindyndns
+    )
+
+    cp lindyndns/build/* "${out_dir}"
 }
 
 windows() { true; }
 
 main() {
-    tmpdir="$(mktemp -d)"
-    tag=""
-    if [ -n "${TRAVIS_TAG}" ]; then tag="-${TRAVIS_TAG}"; fi
-    name="${PROJECT_NAME}${tag}-${TARGET}"
-    staging="${tmpdir}/${PROJECT_NAME}"
-
-    out_dir="$(pwd)/deployment"
-    mkdir -p "${out_dir}" "${staging}"
-
     cp "target/${TARGET}/release/${PROJECT_NAME}" "${staging}/"
 
     "${TRAVIS_OS_NAME}"
